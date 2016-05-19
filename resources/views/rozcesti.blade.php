795b84ae0f23266b700f47519bb2ee0ad65e8409
Jan Drábek <JD-mac@e-77-62.eduroam.fit.cvut.cz>
2016-05-19 17:19:05 +0200

    +Validate rozcesti, user doesn't exist

--- resources/views/rozcesti.blade.php
+++ resources/views/rozcesti.blade.php
@@ -14,13 +14,23 @@
         text-align:center;
         padding-top:1em;
     }
-    
+
     ul{
        list-style-type:none;
     }
-    
+
     ul:not(.nav):hover{
         padding-bottom:2em;
+    }
+
+    .validate_error{
+      margin: auto;
+      width: 60%;
+      padding: 10px;
+    }
+
+    .validate_error{
+      text-align: center
     }
 
 </style>
@@ -37,6 +47,11 @@
                 </button>
             </li>
             <li class="skryte"><input type="username" name="username" placeholder="Uživatelské ID" required></li>
+            @if (count($errors) > 0)
+              <div class="help-block validate_error alert-warning">
+                  <strong class="validate_error_text">Pracovník neexistuje.</strong>
+              </div>
+            @endif
             <li class="skryte"><input type="month" name="datumPracvykaz" required></li>
             <!--type month je v html5 ktery firefox nepodporuje, nutno urpavit-->
             </form>
