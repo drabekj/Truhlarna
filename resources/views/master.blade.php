f4bbd320d82aa38402ba059c0c3d812675e4ed82
Jan Drábek <JD-mac@Jan-MacBook-Pro-6.local>
2016-05-22 14:51:57 +0200

    PDF print web plugin

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -33,7 +33,6 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-        {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
         <style>
             body {
@@ -97,16 +96,10 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
         <script>
-          var app = angular.module('myApp', [], function($interpolateProvider) {
-              $interpolateProvider.startSymbol('<%');
-              $interpolateProvider.endSymbol('%>');
-          });
-
-          app.controller('customersCtrl', function($scope, $http) {
-                then(function (response) {$scope.names = $Objednavky;});
-          });
-
+        function myFunction() {
+            window.print();
+        }
         </script>
-        
+
     </body>
     </html>
--- resources/views/ukolovaMzda.blade.php
+++ resources/views/ukolovaMzda.blade.php
@@ -77,56 +77,12 @@
 </table>
 </div>
 
-<div ng-controller="customersCtrl">
-  <table>
-   <tr ng-repeat="x in names">
-     <td><% x.Name %></td>
-     <td><% x.Country %></td>
-   </tr>
-  </table>
-</div>
-
 <p>
     <div class = "col-md-4" align="center">
             <a href="{{action('RozcestiController@rozcesti')}}"
             class="btn btn-primary btn-lg">Zpět</a>
-            <button onclick="genPDF()"
+            <button onclick="myFunction()"
             class="btn btn-primary btn-lg">Uložit PDF</button>
     </div>
 </p>
-
-<script type="text/javascript" >
-
-  function genPDF(){
-    var pdf = new jsPDF('p', 'mm', [550, 650]);
-
-    source = $('#pdf_content')[0];
-    specialElementHandlers = {
-        '#bypassme': function (element, renderer) {
-           return true
-        }
-    };
-
-    margins = {
-      top: 80,
-      bottom: 60,
-      left: 10,
-      width: 700
-    };
-
-    pdf.fromHTML(
-    source, // HTML string or DOM elem ref.
-    margins.left, // x coord
-    margins.top, { // y coord
-        'width': margins.width, // max width of content on PDF
-        'elementHandlers': specialElementHandlers
-    },
-    function (dispose) {
-      // dispose: object with X, Y of the last line add to the PDF
-      //          this allow the insertion of new lines after html
-      pdf.save('ukolova_mzda.pdf');
-    }, margins);
-  }
-
-</script>
 @stop
